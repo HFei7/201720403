e419addd582562852a9e1c20fadd3592a6571fe4
15690868518 <57735221+15690868518@users.noreply.github.com>
2019-11-27 21:25:23 +0800

    passion

---
+++ src/实验二/SubstractOperation.java
@@ -0,0 +1,14 @@
+package สตั้ถþ;
+
+public class SubstractOperation extends BinaryOperation_3_2 {
+	public SubstractOperation() {	
+		generateBinaryOperation('-');		
+	}	
+	boolean checkingCalculation(int anInteger){
+		return anInteger >= LOWER;
+	}
+	int calculate(int left, int right){
+		return left-right;
+	}
+
+}
